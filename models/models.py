4349c1f7614c0ab06c2fbe065d02ba345bdb7268
PURPLE <purplleee33@gmail.com>
2024-05-02 22:22:46 +0100

    added the users class

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 from uwu.database import db
-
+from flask_login import UserMixin
 
 class Ticket(db.Model):
     __tablename__ = 'tickets'
@@ -16,3 +16,29 @@ class Materiel(db.Model):
     id_mat = db.Column(db.String(10), primary_key=True)
     marque = db.Column(db.String(100), nullable=False)
     typeMat = db.Column(db.String(20), nullable=False)
+
+
+
+class User(UserMixin):
+    def __init__(self, id, roles):
+        self.id = id
+        self.active_role = roles[0]  
+        self.roles = roles
+
+    def switch_role(self, new_role):
+        if new_role in self.roles:
+            self.active_role = new_role
+            return True
+        return False
+
+    @property
+    def is_admin(self):
+        return self.active_role == 'admin'
+
+    @property
+    def is_employee(self):
+        return self.active_role == 'employee'
+
+    @property
+    def is_super_admin(self):
+        return self.active_role == 'super_admin'
